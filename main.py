7078d970e685d03b7271e5bad14e23ba11ac7c8b
AlFuentesDLM <alvaro.fuentes@mercadolibre.cl>
2021-08-31 16:55:46 -0400

    model fix

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 import psutil
 import json
-import os
 import platform
-import subprocess
 from cpuinfo import get_cpu_info
-
+import time
 
 
 def parseUsers():
@@ -16,25 +14,28 @@ def parseUsers():
             {"name": user.name, "terminal": user.terminal, "host": user.host, "pid": user.pid})
     return arrayParsedUsers
 
+
 def getOsName():
     return platform.system()
+
 
 def getOsVersion():
     return platform.release()
 
-def getCpuInfo():
-    cpuinfo = get_cpu_info()
-    obj = {
-             "architecture": cpuinfo["brand_raw"],
-             "cpu_logical_core":psutil.cpu_count(logical=True),
-             "cpu_fisic_core":psutil.cpu_count(logical=False),
-             "brand":cpuinfo["brand_raw"],
-    }
-    return obj
 
 
-listOfprocess = []
-for proc in psutil.process_iter(['pid', 'name', "ppid", "username"]):
-    listOfprocess.append(proc.info)
+def processList():
+    listOfprocess = []
+    for proc in psutil.process_iter(['pid', 'name', "ppid", "username"]):
+        listOfprocess.append(proc.info)
+    return listOfprocess
 
-print(json.dumps({"users":parseUsers(),"process":listOfprocess,"osname":getOsName(),"osversion":getOsVersion(),"cpu":getCpuInfo()}))
+cpuinfo = get_cpu_info()
+
+# send api request
+print(json.dumps({"users": parseUsers(), "process": processList(
+), "os_name": getOsName(), "os_version": getOsVersion(),  "architecture": cpuinfo["brand_raw"],
+        "cpu_logical_cores": psutil.cpu_count(logical=True),
+        "cpu_physical_cores": psutil.cpu_count(logical=False),
+        "brand": cpuinfo["brand_raw"],
+        "timestamp":int(round(time.time()*1000,1))}))
