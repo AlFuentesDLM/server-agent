dccc1d2ec3ca20cf95857ffecea33225d6ef3844
AlFuentesDLM <alvaro.fuentes@mercadolibre.cl>
2021-09-01 10:13:09 -0400

    refining agent

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
+import requests
 import psutil
 import json
 import platform
 from cpuinfo import get_cpu_info
 import time
+import os
 
 
 def parseUsers():
@@ -23,19 +25,44 @@ def getOsVersion():
     return platform.release()
 
 
-
 def processList():
     listOfprocess = []
     for proc in psutil.process_iter(['pid', 'name', "ppid", "username"]):
         listOfprocess.append(proc.info)
     return listOfprocess
 
+
 cpuinfo = get_cpu_info()
 
-# send api request
-print(json.dumps({"users": parseUsers(), "process": processList(
-), "os_name": getOsName(), "os_version": getOsVersion(),  "architecture": cpuinfo["brand_raw"],
+
+def getUrl():
+    try:
+        return os.environ['AUDIT_URL']
+    except:
+        print("set the audit url in the enviroment 'export AUDIT_URL=https....'")
+
+
+def makePostRequest(url,jsonData):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url=url, data=jsonData,headers=headers)
+    if(response.status_code == 201):
+        print("the request was successfully sent to the audit server")
+        re = json.loads(response.content.decode())
+        id = re["audit_id"]
+        print("make a get request to our api to see the results")
+        print(getUrl()+"/"+str(id))
+        print(getUrl()+"/"+str(id)+"/process")
+        print(getUrl()+"/"+str(id)+"/users")
+    else:
+        print("something went wrong")
+
+
+def generateJson():
+    return json.dumps({"users": parseUsers(), "process": processList(
+    ), "os_name": getOsName(), "os_version": getOsVersion(),  "architecture": cpuinfo["arch"],
         "cpu_logical_cores": psutil.cpu_count(logical=True),
         "cpu_physical_cores": psutil.cpu_count(logical=False),
         "brand": cpuinfo["brand_raw"],
-        "timestamp":int(round(time.time()*1000,1))}))
+        "timestamp": int(round(time.time()*1000, 1))})
+# send api request
+makePostRequest(getUrl(),generateJson())
